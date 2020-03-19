99451970d03167ddd4d45e81a4ae9fa678f098cd
langinteger <langinteger@outlook.com>
2020-03-19 23:13:31 +0800

    update

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # Automation
 
-## 1 Utilities
+## 1 Prerequisite
 
 ### 1.1 Python
 
-You know how to install, `python` and `pip` usually got installed together.
+[这里](https://www.python.org/downloads/)下载, `python` and `pip` usually got installed together.
 
 ### 1.2 [Selenium WebDriver](https://www.selenium.dev/projects/)
 
 Selenium WebDriver drives a browser natively, as a real user would, either locally or on remote machines.
 
-- Download from [ChromeDriver](https://chromedriver.chromium.org/downloads) - WebDriver for Chrome
-- Add to `PATH` system envirables
+- [这里](https://chromedriver.chromium.org/downloads)下载 - WebDriver for Chrome，注意选择与自己 chrome 兼容的版本
+- 下载解压后放到脚本文件旁边
 
 ### 1.3 Selenium
 
-Install with `pip3 install selenium`
+通过 pip3 下载：`pip3 install selenium`
 
 ### ~~1.4 PhantomJS~~
 
@@ -36,8 +36,12 @@ then `source ~/.bash_profile` to valid the change.
 
 An alternative for PhantomJS, to get http response code.
 
-Install with `pip3 install requests`.
+通过 pip3 下载：`pip3 install requests`.
 
-## 2 Run
+## 2 Usage
 
-> python3 jenkins_build.py -e dev -s 2 -n tmc-services
+- python3 jenkins_build.py -e dev -s 2 -n tmc-services
+
+or
+
+- python jenkins_build.py -e dev -s 2 -n tmc-services
--- jenkins_build.py
+++ jenkins_build.py
@@ -28,58 +28,71 @@ logging.getLogger('').addHandler(console)
 login_url_pattern = "https://{env}-jenkins.teyixing.com/"
 url_pattern = "https://{env}-jenkins.teyixing.com/view/{env}-tyx-env-{seq}/job/{env}-{name}-env-{seq}"
 
+
 def check_and_get_params():
     text = 'python3 jenkins_build.py'
     parser = argparse.ArgumentParser(text)
-    parser.add_argument("-e", "--env", choices=['dev', 'test'], required=True, help="environment, dev/test")
-    parser.add_argument("-s", "--seq", choices=['1', '2', '3', '4', '5'], required=True, help="namespace sequence, 1/2/3/4")
-    parser.add_argument("-n", "--name", required=True, help="service name, tmc-services")
+    parser.add_argument(
+        "-e", "--env", choices=['dev', 'test'], required=True, help="environment, dev/test")
+    parser.add_argument(
+        "-s", "--seq", choices=['1', '2', '3', '4', '5'], required=True, help="namespace sequence, 1/2/3/4")
+    parser.add_argument("-n", "--name", required=True,
+                        help="service name, tmc-services")
     args = parser.parse_args()
     return args
 
+
 def build_login_url(args):
-    return login_url_pattern.format(env = args.env)
+    return login_url_pattern.format(env=args.env)
+
 
 def build_url(args):
-    return url_pattern.format(env = args.env, seq = args.seq, name = args.name)
+    return url_pattern.format(env=args.env, seq=args.seq, name=args.name)
+
 
 def test_connection(build, args, cookies):
     url = build(args)
-    logging.info("Build jenkins url: {0}".format(url))
+    logging.info("Build url: {0}".format(url))
 
     session = requests.Session()
     if (cookies != None):
         for cookie in cookies:
             session.cookies.set(cookie['name'], cookie['value'])
     response = session.get(url)
-    logging.info("Access jenkins with status: {0}".format(response.status_code))
+    logging.info("Access jenkins with status: {0}".format(
+        response.status_code))
 
     if response.status_code != 200 and response.status_code != 403:
-        logging.error("{0} is not accessible, check you params and run again.".format(url))
+        logging.error(
+            "{0} is not accessible, check you params and run again.".format(url))
         exit(2)
+
 
 def login(args, driver):
     url = build_login_url(args)
     driver.get(url)
-    
+
     driver.find_element_by_name("j_username").send_keys("admin")
     driver.find_element_by_name("j_password").send_keys("111111")
     driver.find_element_by_name("Submit").click()
     return driver.get_cookies()
 
+
 def do_build(args, driver):
     url = build_url(args)
     driver.get(url)
-    
+
     driver.find_element_by_link_text("Build Now").click()
     logging.info("Build started...")
+
 
 def build():
     args = check_and_get_params()
     test_connection(build_login_url, args, None)
-    driver = webdriver.Chrome("D:\software\\binary\chromedriver.exe")
+    driver = webdriver.Chrome()
     cookies = login(args, driver)
     test_connection(build_url, args, cookies)
     do_build(args, driver)
+
 
 build()
