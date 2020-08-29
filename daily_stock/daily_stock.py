15b07855b0f1bba560626149b3bca6b5fb921e58
liulang <langinteger@outlook.com>
2020-08-29 14:05:31 +0800

    feat - 添加上深之外的数据

--- daily_stock/daily_stock.py
+++ daily_stock/daily_stock.py
@@ -15,10 +15,14 @@ data_dicts = json.dumps(
     sort_keys=True)
 print(data_dicts)
 
-DESTINATION = argv[1]
 jsonData = json.loads(page.text.encode('utf-8').decode('utf-8'))
 
-desc = jsonData[0]['Name'] + ' 收：' + jsonData[0]['Price'] + ' 涨跌：' + jsonData[0]['ChangeRate'] + "\n\r\n\r" + jsonData[1]['Name'] + ' 收：' + jsonData[1]['Price'] + ' 涨跌：' + jsonData[1]['ChangeRate']
+desc = jsonData[0]['Name'] + ' 收：' + jsonData[0]['Price'] + ' 涨跌：' + jsonData[0]['ChangeRate'] + "\n\r\n\r" \
+     + jsonData[1]['Name'] + ' 收：' + jsonData[1]['Price'] + ' 涨跌：' + jsonData[1]['ChangeRate'] + "\n\r\n\r" \
+     + jsonData[2]['Name'] + ' 收：' + jsonData[1]['Price'] + ' 涨跌：' + jsonData[1]['ChangeRate'] + "\n\r\n\r" \
+     + jsonData[3]['Name'] + ' 收：' + jsonData[1]['Price'] + ' 涨跌：' + jsonData[1]['ChangeRate'] + "\n\r\n\r" \
+     + jsonData[4]['Name'] + ' 收：' + jsonData[1]['Price'] + ' 涨跌：' + jsonData[1]['ChangeRate'] + "\n\r\n\r" \
+     + jsonData[5]['Name'] + ' 收：' + jsonData[1]['Price'] + ' 涨跌：' + jsonData[1]['ChangeRate'] + "\n\r\n\r"
 params = {'text':'每日财经数据','desp':desc}
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"}
-requests.get(DESTINATION, params = params, headers = headers)
+requests.get(argv[1], params = params, headers = headers)
