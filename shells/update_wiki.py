d821959395ee4390f60264493a0de0f68846465e
liulang <709091988@qq.com>
2023-06-02 01:40:45 +0800

    feat: update py

--- shells/update_wiki.py
+++ shells/update_wiki.py
@@ -15,7 +15,7 @@ def process_input_string(input_string):
     for line in lines:
         strip_line = line.strip()
         if strip_line.startswith("- [x]") and strip_line.endswith("service"):
-            changed_services.add(strip_line.string.rsplit(None, 1)[-1])
+            changed_services.add(strip_line.rsplit(None, 1)[-1])
     return changed_services
 
 def merge_changes_to_file(input_string, directory, change):
@@ -25,14 +25,15 @@ def merge_changes_to_file(input_string, directory, change):
     file_path = os.path.join(directory, file_name)
     print("file path is " + file_path)
 
-    if not os.path.exists(file_path):
-        os.makedirs(directory, exist_ok=True)
-        with open(file_path, 'w') as f:
-            pass
+    try:
+        file = open(file_path, "x")
+        file.close()
+    except FileExistsError:
+        print(f"The file '{file_path}' already exists.")
         
     service_changed = process_input_string(input_string)
-    print("service changed: " + service_changed)
-    with open(file_path, 'w') as f:
+    print("service changed: " + str(service_changed))
+    with open(file_path, 'r+') as f:
         value = f.readlines()
         ast = markdown_to_json.CommonMark.DocParser().parse(value)
         dictionary = markdown_to_json.CMarkASTNester().nest(ast)
@@ -42,13 +43,15 @@ def merge_changes_to_file(input_string, directory, change):
                 dictionary[service].append(change)
             else:
                 dictionary[service] = [change]
-        print("merged changes: " + dictionary)
+        print("merged changes: " + str(dictionary))
         output_str = ""
         for key in sorted(dictionary.keys()):
             output_str += "# " + key + "\n"
             values_str = "\n".join(sorted(dictionary[key]))
             output_str += values_str + "\n"
         print("finally markdown: " + output_str)
+        # Move the file pointer to the beginning of the file
+        file.seek(0)
         f.write(output_str)
 
 if __name__ == '__main__':
@@ -60,6 +63,29 @@ if __name__ == '__main__':
     directory = sys.argv[2]
     pr_url = sys.argv[3]
     pr_title = sys.argv[4]
+
+#     input_string = """
+#     ### Summary
+# What does this PR do?
+
+# ### Does this close any open issues?
+# Closes xx
+
+# ### Screenshots
+# Include any relevant screenshots here.
+
+# ### Affected Services
+
+# - [ ] demo_service
+# - [x] a_service
+# - [x] b_service
+
+# ### Other Information
+# Any other information that is important to this PR.
+#     """
+#     directory = "/Users/liulang/gitrepo/langinteger/automation"
+#     pr_url = "https://api.github.com/repos/LangInteger/automation/pulls/9"
+#     pr_title = "xx"
 
     last_slash_index = pr_url.rfind("/")
     pr_no = pr_url[last_slash_index + 1:]
